a6166be15f797ed40e970e9a35ecc7f512745548
Ryan <ryan.y.an@qq.com>
2018-03-28 13:27:15 +0800

    test commit

--- src/index.js
+++ src/index.js
@@ -6,5 +6,7 @@ process.on('uncaughtException', err => {
     console.log("ERR / " + '500 '.red + err.message);
 });
 
+console.log('Welcome to Sausage Server');
+
 server.listen(V.server.port);
 console.log('SER / ' + '200'.green + ` Server started, listened port: ${V.server.port}`);
